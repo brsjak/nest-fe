6227fdfed9be6502f7f4e933b732c9469f4c1afa
Filip <brsjakproductions@gmail.com>
2021-02-15 15:37:39 +0100

    nest-fe/basic unit tests added

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import MainContainer from "./Containers/MainContainer/MainContainer";
+
+function App() {
+  return <MainContainer />;
+}
+
+export default App;
---
+++ src/Containers/MainContainer/MainContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Calculator } from "../../Components/Calculator/Calculator";
+
+const MainContainer = () => {
+  return (
+    <>
+      <h1>Hello world!</h1>
+      <Calculator />
+    </>
+  );
+};
+
+export default MainContainer;
